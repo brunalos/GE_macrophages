a1533469c7e2fbd32abe32ab093d2ee17eef9b02
Bruna Los <brunalos17@gmail.com>
2024-06-28 16:44:50 +0200

    added new file

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from shiny.express import ui
+from pathlib import Path
+import pandas as pd
+import plotly.express as px
+from shiny import App, Inputs, Outputs, Session, render, ui, reactive
+from shiny.express import ui, render, input
+from shinywidgets import render_plotly
 
+# Set up UI options
+
+ui.page_opts(title="Gene Expression", fillable=True)
 ui.page_opts(fillable=True, id="page")
 
-ui.nav_spacer()  
-with ui.nav_control():  
-    ui.input_dark_mode() 
+
+# Read the file
+@reactive.calc
+def dataframe():
+    infile = Path(__file__).parent / "data/GE.csv"
+    return pd.read_csv(infile)
+
+
